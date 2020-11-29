91a0c7b8cea63958c63ac4716b2b8be947350040
EC2 Default User <ec2-user@ip-172-31-2-22.ap-south-1.compute.internal>
2020-11-29 05:40:50 +0000

    added support for adding multiple roles and permissions for a role

--- CreatePermission/build.sh
+++ CreatePermission/build.sh
@@ -6,5 +6,5 @@
     zip -r ../auth_service_create_permission.zip .
     
     
-    aws lambda create-function --function-name auth_service_create_permission --runtime nodejs12.x  --handler CreatePermission/index.handler --publish --zip-file fileb://auth_service_create_permission.zip
+    aws lambda create-function --function-name auth_service_create_permission --runtime nodejs12.x --role  --handler CreatePermission/index.handler --publish --zip-file fileb://auth_service_create_permission.zip
     
